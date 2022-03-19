a95597ef34b4bbf31d351641e637b138f29db7d7
owlsuri <imsimple2563@gmail.com>
2022-03-19 23:03:03 +0900

    잇츠로드 1차완성

--- eatsroad/pages/index.js
+++ eatsroad/pages/index.js
@@ -1,9 +1,25 @@
 import { useState } from 'react'
-import {Wrap, LocationImg, Header, Title,
-        Bar, User, EmailBox, Email, DeleteImg, 
-        PasswordBox, Password, Error,
-        LoginBtn, LabelBox, Label, LabelDiv,
-        KakaoBox, KakaoImg,KakaoText}from '../styles/eatsroad'
+import {
+  Container, Wrap,
+  LocationImg,
+  Header,
+  Title,
+  Bar,
+  User,
+  EmailBox,
+  Email,
+  DeleteImg,
+  PasswordBox,
+  Password,
+  Error,
+  LoginBtn,
+  LabelBox,
+  Label,
+  LabelDiv,
+  KakaoBox,
+  KakaoImg,
+  KakaoText,
+} from "../styles/eatsroad";
 
 export default function eatsRoad(){
   const [email, setEmail] = useState("")
@@ -16,7 +32,7 @@ export default function eatsRoad(){
     setEmail(event.target.value)
   }
 
-  const onChangePassword = () => {
+  const onChangePassword = (event) => {
     setPassword(event.target.value)
   }
 
@@ -24,51 +40,65 @@ export default function eatsRoad(){
         let check = true;
         
         if(email.includes("@") === false){
-            setEmailError("이메일이 올바르지 않습니다.")
+            setEmailError("이메일 주소를 다시 확인해주세요.")
             check = false;
         } else{
             setEmailError("")
         }
 
-        if(password == ""){
-          setPasswordError("비밀번호를 입력해주세요")
+        if(password == "" || password.length < 8){
+          setPasswordError("8~16자의 영문, 숫자, 특수문자만 사용 가능합니다.")
           check = false;
         } else{
           setPasswordError("")
         }
+        if(check === true){
+          alert("환영합니다!")
+        }
   }
 
     return (
-      <Wrap src="/shaian-ramesht-exSEmuA7R7k-unsplash.jpg">
-        <Header>
-        <LocationImg src="/img-100-logo-white@3x.png"/>
-        <Bar></Bar>
-        <Title>잇츠로드</Title>
-        </Header>
-        <User>
-          <EmailBox>
-            <Email type='text' placeholder="이메일을 입력하세요."/>
-            <DeleteImg src="/ic-20-delete-white@2x.png"/>
+      <Container>
+        <Wrap>
+          <Header>
+            <LocationImg src="/img-100-logo-white@3x.png" />
+            <Bar></Bar>
+          </Header>
+          <Title>잇츠로드</Title>
+          <User>
+            <EmailBox>
+              <Email
+                type="text"
+                placeholder="이메일을 입력하세요."
+                onChange={onChangeEmail}
+              />
+              <DeleteImg
+                src="/ic-20-delete-white@2x.png" />
+            </EmailBox>
             <Error>{emailError}</Error>
-          </EmailBox>
-          <PasswordBox>
-            <Password type="password" placeholder="비밀번호를 입력하세요."/>
-            <DeleteImg src="/ic-20-delete-white@2x.png"/>
+            <PasswordBox>
+              <Password
+                type="password"
+                placeholder="비밀번호를 입력하세요."
+                onChange={onChangePassword}
+              />
+              <DeleteImg src="/ic-20-delete-white@2x.png" />
+            </PasswordBox>
             <Error>{passwordError}</Error>
-          </PasswordBox>
-          <LoginBtn onClick="">로그인</LoginBtn>
-          <LabelBox>
-            <Label>이메일 찾기</Label>
-            <LabelDiv></LabelDiv>
-            <Label>비밀번호 찾기</Label>
-            <LabelDiv></LabelDiv>
-            <Label>회원가입</Label>
-          </LabelBox>
-          <KakaoBox>
-            <KakaoImg src="/kakao-talk.png"/>
-            <KakaoText>카카오톡으로 로그인</KakaoText>
-          </KakaoBox>
-        </User>
-      </Wrap>
-    )
+            <LoginBtn onClick={onClickLoginBtn}>로그인</LoginBtn>
+            <LabelBox>
+              <Label>이메일 찾기</Label>
+              <LabelDiv></LabelDiv>
+              <Label>비밀번호 찾기</Label>
+              <LabelDiv></LabelDiv>
+              <Label>회원가입</Label>
+            </LabelBox>
+            <KakaoBox>
+              <KakaoImg src="/kakao-talk.png" />
+              <KakaoText>카카오톡으로 로그인</KakaoText>
+            </KakaoBox>
+          </User>
+        </Wrap>
+      </Container>
+    );
   }
--- eatsroad/styles/eatsroad.js
+++ eatsroad/styles/eatsroad.js
@@ -1,64 +1,144 @@
 import styled from "@emotion/styled"
 
-export const Wrap = styled.div`
-    width: 640;
+export const Container = styled.div`
+    width: 640px;
     height: 1138px;
+    background-image: url("/shaian-ramesht-exSEmuA7R7k-unsplash.jpg");
+    background-size: cover;
+`;
 
+export const Wrap = styled.div`
+padding: 224px 50px 0 50px;
+    width: 640px;
+    height: 1138px;
     background-color: rgba(0,0,0,0.55);
 
     `
-
-export const LocationImg = styled.img`
-
-    `
 export const Header = styled.div`
+position: relative;
 
-    `
-export const Title = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
+`;
+export const LocationImg = styled.img`
+    width: 100px;
+    height: 100px;
     `
 export const Bar = styled.div`
-
+    position: absolute;
+    top: 83px;
+    width: 50px;
+    height: 15px;
+    border-radius: 10px;
+    background-color: rgba(255,255,255,0.5);
+`;
+export const Title = styled.div`
+    color: white;
+    font-size: 60px;
+    text-align: center;
+    font-weight: 600;
     `
-export const User = styled.div`
 
+export const User = styled.div`
+    padding-top: 140px;
     `
 export const EmailBox = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
     `
 export const Email = styled.input`
-
-    `
+    width: 500px;
+    height: 50px;
+    border-top: 0;
+    border-left: 0;
+    border-right: 0;
+    border-bottom-color: rgba(255,255,255,0.5);
+    background: none;
+    font-size: 24px;
+    color: white;
+`;
 export const Error = styled.div`
-
-    `
+    padding: 7px 0 0 13px;
+    color: rgba(255, 27, 109, 0.7);
+    font-size: 18px;
+    font-weight: 600;
+    `;
 export const DeleteImg = styled.img`
-
+    width: 20px;
+    height: 20px;
     `
 export const PasswordBox = styled.div`
-
-    `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-top: 50px;
+`;
 export const Password = styled.input`
-
-    `
+    width: 500px;
+    height: 50px;
+    border-top: 0;
+    border-left: 0;
+    border-right: 0ch;
+    border-bottom-color: rgba(255, 255, 255, 0.5);
+    background: none;
+    font-size: 24px;
+    color: white;
+`;
 export const LoginBtn = styled.button`
-
-    `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+    width: 540px;
+    height: 76px;
+    border-radius: 38px;
+    color: white;
+    font-weight: 700;
+    font-size: 26px;
+    border: none;
+    background-color: rgba(255, 27, 109, 0.6);
+    cursor: pointer;
+`;
 export const LabelBox = styled.div`
+    padding-top: 50px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+
 
     `
 export const Label = styled.div`
-
-    `
+    color: white;
+    font-size: 20px;
+`;
 export const LabelDiv = styled.div`
-
+    width: 1px;
+    height: 12px;
+    background-color: rgba(255,255,255,0.5);
     `
 export const KakaoBox = styled.div`
-
-    `
+    margin-top: 50px;
+    width: 540px;
+    height: 76px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid rgba(255,214,0,0.55);
+    border-radius: 38px;
+`;
 export const KakaoImg= styled.img`
-
+    width: 28px;
     `
 export const KakaoText = styled.div`
-
-    `
+    color: #fae100;
+    padding-left: 20px;
+    font-size: 26px;
+    font-weight: 700;
+`;
